fc54f7335cbbb5fc252c69ba3e802849bcd185d8
Kevin Faaborg <zootella@zootella.com>
2025-06-08 14:01:02 -0600

    recloned

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 <p>This HTML comes from a file at <code>./src/routes/+page.svelte</code></p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>Preparing this minimal site for <i>oauth2.ara.team</i></p>
-<p>List configured OAuth providers at <a href="/auth/providers">/auth/providers</a></p>
+<p>List configured OAuth providers at <a href="/auth/providers">/auth/providers</a> and then</p>
 <div><form method="POST" action="/auth/signin/google"><button type="submit">Continue with Google</button></form></div>
 <div><form method="POST" action="/auth/signin/twitter"><button type="submit">Continue with Twitter</button></form></div>
 </main>

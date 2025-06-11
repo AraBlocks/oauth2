f231b5c4d795e5e97d174ae75753036302e8b1a1
Kevin Faaborg <zootella@zootella.com>
2025-06-11 16:47:53 -0600

    history hack

--- src/routes/continue/[provider]/+page.svelte
+++ src/routes/continue/[provider]/+page.svelte
@@ -7,10 +7,12 @@ this page shows no content at all; our goal is it's invisible to the user
 */
 
 import {onMount} from 'svelte'
-import {page} from '$app/stores'
-import {get} from 'svelte/store'
+import {page} from '$app/stores'//get svelte's page store, which holds the url and parameters
+import {get} from 'svelte/store'//svelte stores are reactive, so we need this api to read a value imperatively
 
-onMount(() => {
+onMount(() => {//run once the browser has rendered this page, and never on the server
+
+	history.replaceState(null, '', location.href)//prevent this page from being a stop in the browser's Back history
 
 	const provider = get(page).params.provider
 

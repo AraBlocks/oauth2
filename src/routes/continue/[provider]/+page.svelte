2731db8d32b7d499c063d05a59b6c938cfb22a57
Kevin Faaborg <zootella@zootella.com>
2025-06-11 16:03:37 -0600

    intermediate continue page flow works

---
+++ src/routes/continue/[provider]/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+/*
+in the [oauth2.ara.team] [SvelteKit] repo using [@auth/sveltekit],
+this is the file ./src/routes/continue/[provider]/+page.svelte
+
+this page shows no content at all; our goal is it's invisible to the user
+*/
+
+import {onMount} from 'svelte'
+import {page} from '$app/stores'
+import {get} from 'svelte/store'
+
+onMount(() => {
+
+	const provider = get(page).params.provider
+
+	const form = document.createElement('form')
+	form.method = 'POST'
+	form.action = `/auth/signin/${provider}`
+
+	form.style.display = 'none'//hide the empty form node to avoid flashing the DOM
+	document.body.append(form)
+	form.submit()
+})
+
+</script>
